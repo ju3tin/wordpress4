d08322d429b9e866d013953420b3a8ff190cd9c4
Justin Grierson <ju3tin@hotmail.co.uk>
2017-09-07 16:50:46 +0100

    testchargebee

--- login5/dbconnect.php
+++ login5/dbconnect.php
@@ -1,9 +1,9 @@
 <?php
 
-$db_host = "localhost";
-$db_name = "phirstcv";
-$db_user = "root";
-$db_pass = "";
+$db_host = "eu-mm-auto-dub-01-b.cleardb.net";
+$db_name = "heroku_70596e6cfc15edd";
+$db_user = "b71ccb4ea22b98";
+$db_pass = "d815a370";
 
 
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
