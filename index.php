7ed594e200563014f15fa95bb24a2a2d82a57fdb
Justin Grierson <ju3tin@hotmail.co.uk>
2017-09-14 14:31:02 +0100

    help me

--- index.php
+++ index.php
@@ -27,6 +27,17 @@
     <!-- Custom styles for this template -->
     <link href="css/new-age.min.css" rel="stylesheet">
 
+
+<script>
+$(document).ready(function(){
+$("#dudet").hide();
+    $("#red").click(function(){
+        $("#dude").hide();
+        $("#dudet").show();
+    });
+});
+</script>
+
   </head>
 
   <body id="page-top">
@@ -185,13 +196,140 @@ if (isset($_POST['btn-login'])) {
                 </div>
 
                 <div class="form-group">
-                    <a href="register.php" type="button" class="btn btn-block btn-danger"
-                       name="btn-login">Register</a>
+                    <a href="#" type="button" class="btn btn-block btn-danger"
+                  id="red"     name="btn-login">Register</a>
                 </div>
 
             </div>
 
         </form>
+        
+        
+        
+        <?php
+
+if (isset($_SESSION['user']) != "") {
+    header("Location: index.php");
+}
+include_once 'login5/dbconnect.php';
+
+if (isset($_POST['signup'])) {
+
+    $uname = trim($_POST['uname']); // get posted data and remove whitespace
+    $email = trim($_POST['email']);
+    $upass = trim($_POST['pass']);
+
+    // hash password with SHA256;
+    $password = hash('sha256', $upass);
+
+    // check email exist or not
+    $stmt = $conn->prepare("SELECT email FROM users WHERE email=?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $stmt->close();
+
+    $count = $result->num_rows;
+
+    if ($count == 0) { // if email is not found add user
+
+
+        $stmts = $conn->prepare("INSERT INTO users(username,email,password) VALUES(?, ?, ?)");
+        $stmts->bind_param("sss", $uname, $email, $password);
+        $res = $stmts->execute();//get result
+        $stmts->close();
+
+        $user_id = mysqli_insert_id($conn);
+        if ($user_id > 0) {
+            $_SESSION['user'] = $user_id; // set session and redirect to index page
+            if (isset($_SESSION['user'])) {
+                print_r($_SESSION);
+                header("Location: index.php");
+                exit;
+            }
+
+        } else {
+            $errTyp = "danger";
+            $errMSG = "Something went wrong, try again";
+        }
+
+    } else {
+        $errTyp = "warning";
+        $errMSG = "Email is already used";
+    }
+
+}
+?>
+
+<div id="dudet">
+ <form method="post" autocomplete="off">
+
+            <div class="col-md-12">
+
+                <div class="form-group">
+                    <h2 class="">Register for our Website</h2>
+                </div>
+
+                <div class="form-group">
+                    <hr/>
+                </div>
+
+                <?php
+                if (isset($errMSG)) {
+
+                    ?>
+                    <div class="form-group">
+                        <div class="alert alert-<?php echo ($errTyp == "success") ? "success" : $errTyp; ?>">
+                            <span class="glyphicon glyphicon-info-sign"></span> <?php echo $errMSG; ?>
+                        </div>
+                    </div>
+                    <?php
+                }
+                ?>
+
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+                        <input type="text" name="uname" class="form-control" placeholder="Enter Username" required/>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
+                        <input type="email" name="email" class="form-control" placeholder="Enter Email" required/>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+                        <input type="password" name="pass" class="form-control" placeholder="Enter Password"
+                               required/>
+                    </div>
+                </div>
+
+                <div class="checkbox">
+                    <label><input type="checkbox" id="TOS" value="This"><a href="#">I agree with
+                            terms of service</a></label>
+                </div>
+
+                <div class="form-group">
+                    <button type="submit" class="btn    btn-block btn-primary" name="signup" id="reg">Register</button>
+                </div>
+
+                <div class="form-group">
+                    <hr/>
+                </div>
+
+                <div class="form-group">
+                    <a href="login.php" type="button" class="btn btn-block btn-success" name="btn-login">Login</a>
+                </div>
+
+            </div>
+
+        </form>
+<div>
     </div>
     
     
